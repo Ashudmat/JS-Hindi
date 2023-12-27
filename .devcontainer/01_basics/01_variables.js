060a16314c66cfaeeeb6dac94d5cbb5af1a97f4d
Ashutosh Sharma <71635584+Ashudmat@users.noreply.github.com>
2023-12-27 21:30:07 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 143562
+let accountEmail = "ashutoshsharmamp812@gmail.com"
+var accountPassword = "12345"
+accountCity = "Varanasi" //since js is safe that's why 
+let accountState
+
+
+
+// accountId = 2  // not allowed
+console.log(accountId);
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21342"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]) // console.table to print multiple values
+
+// Notes:---
+/*
+    (1)Prefer not to use "var" because of issue in block scope and functional scope
+    (2)Semi-colon is not compulsory in js
+*/
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict" // treat all Js code as newer version
+
+// alert (3+3) // we are using Node.js, not browser
+
+console.log(3+3)
+
+console.log("Ashutosh");
+
+let name = "Ashu"
+let age = 24
+let isLoggedIn = false
+
+// Primitive DataTypes:-----
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+//Object:-------
+console.log(typeof"Ashutosh");
+console.log(typeof age);
+console.log(typeof null);  // Null is of object type
+console.log(typeof undefined); // undefined is of undefined type
+
+// Notes:-----
+/*
+   (1) "use strict" is used to treat all js code as newer version
+   (2) "code readability" should be high
+   (3) "alert" will be used in upcoming lectures
+   (4) tc39 and mdn
+*/
