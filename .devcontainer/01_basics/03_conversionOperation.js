177969ae0d77a3d4a3feeec1f526a677f4c10543
Ashutosh Sharma <71635584+Ashudmat@users.noreply.github.com>
2023-12-28 20:40:21 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+let age = 33
+//console.log(typeof age);
+//console.log(typeof (age));
+
+
+
+let score="33"
+//console.log(typeof score);
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+
+
+let strscore="33abc"
+let valueInNumbers = Number(strscore)
+//console.log(typeof valueInNumbers);
+//console.log(valueInNumbers);
+
+
+
+let temp = null
+let valueInNumberrss = Number(temp)
+//console.log(typeof valueInNumberrss)
+//console.log(valueInNumberrss); 
+
+
+
+let temporary = undefined
+let value = Number(temporary)
+//console.log(typeof value)
+//console.log(value); 
+
+
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+
+
+let empty = ""
+let EmptyLoggedIn = Boolean(empty)
+console.log(typeof EmptyLoggedIn);
+console.log(EmptyLoggedIn);
+
+
+
+let notempty = "Ashu"
+let NotEmptyLoggedIn = String(notempty)
+console.log(typeof NotEmptyLoggedIn);
+console.log(NotEmptyLoggedIn);
