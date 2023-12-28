8f36bd8fcb6b56f189ac0f178df9b405acca1bde
Ashutosh Sharma <71635584+Ashudmat@users.noreply.github.com>
2023-12-28 21:33:37 +0000

    comparision

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+/*
+console.log(2>1);
+console.log(2>=1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+*/
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);                   /* A
+// console.log(null == 0);                     v
+// console.log(null <= 0);                     0
+//                                             I
+// console.log(undefined < 0);                 D
+// console.log(undefined == 0);             */
+// console.log(undefined <= 0);
+
+// Strict Check //
+console.log("2"==2); // true
+console.log("2"===2); // false
