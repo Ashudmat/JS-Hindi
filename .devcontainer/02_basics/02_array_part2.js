0d922d670cd39a2d0b874c9eb57e6a38c8e6fd03
Ashutosh Sharma <71635584+Ashudmat@users.noreply.github.com>
2023-12-31 12:17:20 +0000

    array done

--- .devcontainer/02_basics/02_array_part2.js
+++ .devcontainer/02_basics/02_array_part2.js
@@ -5,10 +5,10 @@ const dc_heroes = ["Flash","Kira","Muggles"]
 // console.log(marvel_heroes);   // push operation works directly on existing array
 
 const allHeros = marvel_heroes.concat (dc_heroes)
-// console.log(allHeros);   // concat operation returns a new array
+// console.log(allHeros);       // concat operation returns a new array
 
 
-const all_new_heroes = [...marvel_heroes,...dc_heroes] // spread operation ... does the same thing as concat
+const all_new_heroes = [...marvel_heroes,...dc_heroes]    // spread operation ... does the same thing as concat
 // console.log(all_new_heroes);                           // preferred 
 
 const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
@@ -22,4 +22,4 @@ const real_another_array = another_array.flat(Infinity)
 let score1 =100
 let score2 = 200
 let score3 = 300
-console.log(Array.of(score1,score2,score3));      ///// same as from 
+console.log(Array.of(score1,score2,score3));     ///// same as from 
