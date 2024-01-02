15b53cd31acf2a1b02fb1660f7117e9eac3ebf5b
Ashutosh Sharma <71635584+Ashudmat@users.noreply.github.com>
2024-01-02 22:10:05 +0000

    function$ done

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -30,6 +30,8 @@ function addTwoNumbers3(num1,num2){
 const output3 = addTwoNumbers3(9,9)
 // console.log("Output:",output3);
 
+
+
 function loginUserNotification(username){
     if(username === undefined){
         console.log("Invalid Username");
@@ -37,9 +39,9 @@ function loginUserNotification(username){
     }
     return `${username} just logged in successfully`
 }
-console.log(loginUserNotification("Ashutosh"))
-console.log(loginUserNotification(""))
-console.log(loginUserNotification())
+// console.log(loginUserNotification("Ashutosh"))
+// console.log(loginUserNotification(""))
+// console.log(loginUserNotification())
  
 
 
@@ -47,5 +49,55 @@ function loginUser(username = "sam"){       //if did like this way then in worst
     
     return `${username} just logged in successfully`
 }
-console.log(loginUser())
+// console.log(loginUser())
 // console.log(loginUser("ashu"))   //if passed in arguments then its value will be overridden from sam to ashu
+
+
+function calculateCartPrice(num1){    
+    return num1
+}
+// console.log(calculateCartPrice(69));
+
+/*What if parameters are dynamic*/
+function calculateCartPriceUnfixedArg(...num1){    //rest operator //since we don't know how many items will be there in cart
+    return num1                                    //now we can give as many arguments as required
+}
+// console.log(calculateCartPriceUnfixedArg(200,400,500));
+
+/*Some developers use this as well */
+function calculateCartPriceWithVal(val1,val2,...num1){ //val1=200 //val2=400 // num1=[500,2000]
+    return num1                                    
+}
+// console.log(calculateCartPriceWithVal(200,400,500,2000));
+
+
+/*Handling Objects via functions*/
+const myobject = {
+    username:"elonmuskfragrance",
+    id:"1001",
+    price:"799"
+}
+
+function handleObjects(anyobject){
+    // console.log(`Hi! my username is: ${anyobject.username} and i have been given an id:${anyobject.id} my price:${anyobject.price}`);
+}
+
+handleObjects(myobject)
+
+// Or rather than creating objects we can do it like this way as well
+handleObjects({
+    username:"sam",
+    id:"2000",
+    price:"199"
+})
+
+/*Handling Arrays via funtions*/
+const myCurrentArray = [199,299,399,499,599]
+
+function returnSecondValue(anyarray){
+    return anyarray[1]
+}
+console.log(returnSecondValue(myCurrentArray));
+
+//Or rather than creating arrays we can do it like this way as well
+console.log(returnSecondValue([100,200,300,400]));
