1549a62e985cd45f87c0fcb4d937a3b2abdceae4
Ashutosh Sharma <ashutoshsharmamp812@gmail.com>
2024-01-08 23:03:35 +0000

    filter

--- .devcontainer/05_iterations/06_filter.js
+++ .devcontainer/05_iterations/06_filter.js
@@ -36,7 +36,7 @@ myNums.forEach( (num) => {
         newNums3.push(num)
     }
 } )
-console.log(newNums3);
+// console.log(newNums3);
 
 /**lets create a dummy database and use filter() method **/
 const allBooks = [
@@ -44,16 +44,16 @@ const allBooks = [
         title:'Book-One ', genre:'Fiction', publishYear:1987, edition:2010
     },
     {
-        title:'Book-Two ', genre:'History', publishYear:1987, edition:2010
+        title:'Book-Two ', genre:'History', publishYear:1975, edition:2010
     },
     {
-        title:'Book-Three ', genre:'Horror', publishYear:1987, edition:2010
+        title:'Book-Three ', genre:'Horror', publishYear:1967, edition:2010
     },
     {
-        title:'Book-Four ', genre:'Sci-fi', publishYear:1987, edition:2010
+        title:'Book-Four ', genre:'Sci-fi', publishYear:1999, edition:2010
     },
     {
-        title:'Book-Five ', genre:'Fiction', publishYear:1987, edition:2010
+        title:'Book-Five ', genre:'Fiction', publishYear:2000, edition:2010
     },
     {
         title:'Book-Six ', genre:'History', publishYear:1987, edition:2010
@@ -62,17 +62,22 @@ const allBooks = [
         title:'Book-Seven ', genre:'Horror', publishYear:1987, edition:2010
     },
     {
-        title:'Book-Eight ', genre:'Sci-fi', publishYear:1987, edition:2010
+        title:'Book-Eight ', genre:'Sci-fi', publishYear:2013, edition:2020
     },
     {
-        title:'Book-Nine ', genre:'Fiction', publishYear:1987, edition:2010
+        title:'Book-Nine ', genre:'Fiction', publishYear:1947, edition:2010
     },
     {
-        title:'Book-Ten ', genre:'History', publishYear:1987, edition:2010
+        title:'Book-Ten ', genre:'History', publishYear:1934, edition:2010
     }
 ]
 //Q. Find all books whose genre is History?
 //A....
-
 const userBooks = allBooks.filter( (bk) => bk.genre === 'History' )
-console.log(userBooks);
+// console.log(userBooks);
+
+
+//Q. Find all books which are published after 2000 ?
+//A....
+const userBooks2 = allBooks.filter( (bk) => bk.publishYear >= 2000 )
+console.log(userBooks2);
